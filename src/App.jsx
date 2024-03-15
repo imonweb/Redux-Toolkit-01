83da53d5dc43a7a6f53dac49222d46224e7cc96f
Imon <contact@imonweb.co.uk>
2024-03-15 21:27:14 +0000

    redux-toolkit dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Product from './components/Product'
 
 function App() {
 
+  const router = createBrowserRouter(createRoutesFromElements(
+  <Route>
+    <Route></Route>
+  </Route>
+  ))
   return (
     <>
       <div>
