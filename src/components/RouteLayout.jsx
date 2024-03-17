c0402e3acadcf725decc38b068837d376942a20b
Imon <contact@imonweb.co.uk>
2024-03-17 23:15:38 +0000

    store

--- src/components/RouteLayout.jsx
+++ src/components/RouteLayout.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
 import NavBar from './NavBarPanel'
+import {Provider} from 'react-redux'
+import store from '../store/store'
 
 const RouteLayout = () => {
   return (
-    <>
+    <Provider store={store}>
+ 
     <NavBar />
 
     <main>
       <Outlet />
     </main>
-    </>
+    </Provider>
+ 
   )
 }
 
